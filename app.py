7bb21692c4bffa3f624050a03c150f5a14a0328d
Ishita <nope.ishita@gmail.com>
2025-01-25 12:18:11 +0530

    first commit

---
+++ app.py
@@ -0,0 +1,32 @@
+from flask import Flask, render_template, redirect, url_for, request
+
+#creating an instance of class
+app = Flask(__name__)
+
+#home page
+@app.route("/")
+def home():
+    return render_template("index.html")
+
+
+@app.route("/login", methods=["POST", "GET"])
+def login():
+    if request.method == "POST":
+        user = request.form["nm"]
+        return redirect(url_for("user", usr = user))
+    else:
+        return render_template("login.html")
+
+@app.route("/<usr>")
+def user(usr):
+    return f"<h1>{usr}</h1>"
+
+
+
+if __name__ == "__main__":
+    app.run()
+<<<<<<< HEAD
+=======
+
+app.py
+>>>>>>> 4a0b302c487586c8a3a3f71cd577648aae00dd7a
