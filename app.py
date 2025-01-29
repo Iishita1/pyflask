cc693230fafc4484fa00ce3f91d4ec6463e5dc4f
Ishita <nope.ishita@gmail.com>
2025-01-29 12:22:10 +0530

    new

--- app.py
+++ app.py
@@ -1,32 +1,39 @@
-from flask import Flask, render_template, redirect, url_for, request
+from flask import Flask, render_template, redirect, url_for, request, session
+from datetime import timedelta
 
-#creating an instance of class
 app = Flask(__name__)
+app.secret_key = "hello"
+app.permanent_session_lifetime = timedelta(days=1)
 
-#home page
 @app.route("/")
 def home():
-    return render_template("index.html")
-
+    user = session.get("user")
+    return render_template("index.html", user=user)
 
 @app.route("/login", methods=["POST", "GET"])
 def login():
     if request.method == "POST":
+        session.permanent = True
         user = request.form["nm"]
-        return redirect(url_for("user", usr = user))
+        session["user"] = user
+        return redirect(url_for("user"))
     else:
+        if "user" in session:
+            return redirect(url_for("user"))
         return render_template("login.html")
 
-@app.route("/<usr>")
-def user(usr):
-    return f"<h1>{usr}</h1>"
+@app.route("/user")
+def user():
+    if "user" in session:
+        user = session["user"]
+        return render_template("user.html", user=user)
+    else:
+        return redirect(url_for("login"))
 
-
+@app.route("/logout")
+def logout():
+    session.pop("user", None)
+    return redirect(url_for("home"))
 
 if __name__ == "__main__":
-    app.run()
-<<<<<<< HEAD
-=======
-
-app.py
->>>>>>> 4a0b302c487586c8a3a3f71cd577648aae00dd7a
+    app.run(port=3000)
